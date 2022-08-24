e6668b47d52c251bd0abd2525a09fe52a567348a
ahaikal <ahaikal@users.noreply.github.com>
2022-08-24 16:37:07 -0500

    printRawData ios and android

--- dist/index.js
+++ dist/index.js
@@ -200,9 +200,14 @@ export var NetPrinter = {
     },
     printRawData: function (text, opts) {
         if (opts === void 0) { opts = {}; }
-        return RNNetPrinter.printRawData(text, function (error) {
-            return console.warn(error);
-        });
+        if (Platform.OS === "ios") {
+            RNNetPrinter.printRawData(text, opts, function (error) { return console.warn(error); });
+        }
+        else {
+            RNNetPrinter.printRawData(text, function (error) {
+                return console.warn(error);
+            });
+        }
     },
     printBill: function (text, opts) {
         if (opts === void 0) { opts = {}; }
